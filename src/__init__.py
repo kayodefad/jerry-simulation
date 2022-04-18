18183447d13645dcbd9ad7889ca3ca5c21112474
Oluwakayode Fadoju <fadojuk@gmail.com>
2022-04-19 00:26:58 +0100

    pushed minor fixes

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import os
 from src.api import api
+from flask_cors import CORS
 
 def create_app(test_config = None):
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
 
     if test_config is None:
         app.config.from_mapping(SECRET_KEY=os.environ.get("SECRET_KEY"))
